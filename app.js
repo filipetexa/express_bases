bc1c7b6efe877440d60dd915474300b1daabbab6
filipe texa <filipeteixeirasouza@gmail.com>
2018-07-26 21:22:09 -0300

    desenvolvimento dos metodos basicos de um servidor web get/post/delete

--- app.js
+++ app.js
@@ -2,13 +2,40 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-require('./functions');
+const funcoes = require('./functions');
 
-app.get('/produtos', function (req, res){
-    res.send({ nome: " Note Book", preco: 123.34})
+app.use(bodyParser.urlencoded({ extended: true }))
+app.get('/produtos', function (req, res, next) {
+    res.send(funcoes.getProdutos());
 })
 
 
+app.get('/produtos/:id', function (req, res) {
+    res.send(funcoes.getProduto(req.params.id))
+})
+
+app.post('/produtos', function (req, res) {
+    const produto = funcoes.postProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    });
+    res.send(produto);
+})
+app.delete('/produtos/:id', function (req, res) {
+    const produto = funcoes.deleteOne(req.params.id)
+    res.send(produto)
+})
+
+app.put('/produtos/:id', function (req, res) {
+
+    const produto = funcoes.upDateOne({
+        id: parseInt(req.params.id),
+        nome: req.body.nome,
+        preco: req.body.preco
+    }
+    );
+    res.send(produto)
+})
 
 app.listen(3000, () => {
     console.log('servidor rodando na porta 3000');
--- functions.js
+++ functions.js
@@ -1,18 +1,33 @@
+const sequence = {
+    _id: 1,
+    get id() { return this._id++ }
+}
+const produtos = {};
+
+function postProduto(produto) {
+    if (!produtos.id) produto.id = sequence.id
+    produtos[produto.id] = produto
+    return produto
+}
 
 function getProdutos() {
-
+    return Object.values(produtos)
 }
 
 function getProduto(id) {
-
+    return produtos[id] || {}
 }
 
-function upDateOne(id) {
-
+function upDateOne(produto) {
+    produtos[produto.id] = produto
+    return produto
 }
 
 function deleteOne(id) {
-
+    const produto = produtos[id]
+    delete produtos[id]
+    return produto
 }
 
-module.exports = { getProduto, getProdutos, upDateOne, deleteOne }
+
+module.exports = { getProduto, getProdutos, upDateOne, deleteOne, postProduto }
